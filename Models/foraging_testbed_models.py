Add theoretical matching index for IdealOptimal - Fix the bug of softmax when temperature is to small

Models/foraging_testbed_models.py
@@ -61,7 +61,15 @@ RIGHT = 1
 global_block_size_mean = 80
 global_block_size_sd = 20
 
-softmax = lambda x, softmax_temperature: np.exp(x/softmax_temperature)/np.sum(np.exp(x/softmax_temperature))  # Accept np.arrays
+def softmax(x,softmax_temperature):
+    max_temp = np.max(x/softmax_temperature)
+    
+    if max_temp > 500:  # To prevent explosion of EXP
+        greedy = np.zeros(len(x))
+        greedy[np.random.choice(np.where(x == np.max(x))[0])] = 1
+        return greedy
+    else:   # Normal softmax
+        return np.exp(x/softmax_temperature)/np.sum(np.exp(x/softmax_temperature))  # Accept np.arrays
 
 
 class Bandit:
@@ -193,6 +201,7 @@ class Bandit:
         p_reward = np.zeros([2,self.n_trials])
         
         self.rewards_IdealOptimal = 0
+        self.log_ratios_IdealOptimal = []
         
         # Fill in trials until the required length
         while n_trials_now < self.n_trials:
@@ -223,12 +232,14 @@ class Bandit:
             # Fill in trials for this block
             p_reward[:, n_trials_now : n_trials_now + n_trials_this_block] = p_reward_this_block.T
             
-            # Calculate theoretical upper bound (ideal-p^-optimal) and the (fixed) choice history of it
-            m_star, p_star = self.get_IdealOptimal_reward_rate(np.max(p_reward_this_block), np.min(p_reward_this_block))
+            # Calculate theoretical upper bound (ideal-p^-optimal) and the (fixed) choice history/matching point of it
+            m_star, p_star, log_ratios = self.get_IdealOptimal_anlytical(p_reward_this_block[0])
             self.rewards_IdealOptimal += p_star * n_trials_this_block
             
+            
+            self.log_ratios_IdealOptimal.append(log_ratios)
+            
             if self.forager in ['IdealOptimal']:
-                
                 # For ideal optimal, given p_0(t) and p_1(t), the optimal choice history is fixed, i.e., {m_star, 1} (p_min > 0)
                 if m_star == np.inf:  # Greedy
                     c_max_this = np.argwhere(p_reward_this_block[0] == np.max(p_reward_this_block))[0]
@@ -248,27 +259,45 @@ class Bandit:
         self.block_size  = np.array(block_size)
         self.p_reward_fraction = p_reward[RIGHT,:] / (np.sum(p_reward, axis = 0))   # For future use
         self.p_reward_ratio = p_reward[RIGHT,:] / p_reward[LEFT,:]   # For future use
+        
+        # Theoretical matching index of IdealOptimal
+        X = np.array(self.log_ratios_IdealOptimal)[:,0]
+        Y = np.array(self.log_ratios_IdealOptimal)[:,1]
+        self.matching_slope_IdealOptimal_theoretical = ((X*Y).mean() - X.mean()*Y.mean()) / ((X**2).mean() - (X.mean())**2)
 
         # We should make it random afterwards
         np.random.seed()
         
-    def get_IdealOptimal_reward_rate(self, p_max, p_min):
+    def get_IdealOptimal_anlytical(self, p_reward):
         '''
         Theoretical upper bound of total rewards collected by the ideal-p^-optimal forager (for 2-arm task)  03/28/2020
         '''
+        p_max = np.max(p_reward)
+        p_min = np.min(p_reward)
+        
         if p_min > 0:
             m_star = np.floor(np.log(1-p_max)/np.log(1-p_min))
             p_star = p_max + (1-(1-p_min)**(m_star + 1)-p_max**2)/(m_star+1)  # Still stands even m_star = *
-            return int(m_star), p_star
+    
+            # We can even compute the matching index analytically
+            log_c_ratio = np.log(m_star)
+            log_r_ratio = np.log(p_max) + np.log(1+m_star-p_max) - np.log(1-(1-p_min)**(m_star+1))
+            
+            # If p0 > p1, log ratio should be negative according to the definition
+            if p_reward[0] > p_reward[1]:
+                log_ratios = [-log_r_ratio, -log_c_ratio]
+            else:
+                log_ratios = [log_r_ratio, log_c_ratio]
+            
+            return int(m_star), p_star, log_ratios
         else:
-            return np.inf, p_max
+            return np.inf, p_max, [np.nan, np.nan]
         
     def choose_ps(self, ps):
         '''
         "Poisson"-choice process
         '''
         ps = ps/np.sum(ps)
-        
         return np.max(np.argwhere(np.hstack([-1e-16, np.cumsum(ps)]) < np.random.rand()))
 
     def act(self):

Models/foraging_testbed_simulate.py
@@ -22,8 +22,8 @@ from foraging_testbed_models import Bandit
 from foraging_testbed_plots import plot_all_reps, plot_para_scan, plot_model_compet
 
 methods = [ 
-            # 'serial',
-            'apply_async'     # Use multiprocessing.apply_async() for parallel computing (8~10x speed-up in my 8/16 I9-9900k)
+            'serial',
+            # 'apply_async'     # Use multiprocessing.apply_async() for parallel computing (8~10x speed-up in my 8/16 I9-9900k)
           ]
 
 LEFT = 0
@@ -224,7 +224,9 @@ def run_sessions_parallel(bandit, n_reps = global_n_reps, pool = '', para_optim 
     
     if not (para_scan or para_optim):
         results_all_sessions['stay_duration_hist'] = np.zeros(len(stay_duration_hist_bins)-1)
-    
+        
+        if bandit[0].forager == 'IdealOptimal':
+            results_all_sessions['matching_slope_IdealOptimal_theoretical_per_session'] = np.zeros(n_reps)
     
     results_all_sessions['foraging_efficiency_per_session'] = np.zeros([n_unique_bandits, n_reps])
     results_all_sessions['linear_fit_income_per_session'] = np.zeros([n_unique_bandits, n_reps])
@@ -249,11 +251,14 @@ def run_sessions_parallel(bandit, n_reps = global_n_reps, pool = '', para_optim 
             # Session-wise
             results_all_sessions['foraging_efficiency_per_session'][unique_idx,ss] = bb.foraging_efficiency
             results_all_sessions['linear_fit_income_per_session'][unique_idx,ss] = bb.linear_fit_income_per_session # Add session-wise matching slope for model competition
-            results_all_sessions['linear_fit_return_per_session'][unique_idx,ss] = bb.linear_fit_return_per_session # Mathcin slope from log_return_ratio
+            results_all_sessions['linear_fit_return_per_session'][unique_idx,ss] = bb.linear_fit_return_per_session # Matching slope from log_return_ratio
             
             if not (para_scan or para_optim):
                 results_all_sessions['stay_duration_hist'] += np.histogram(bb.stay_durations, bins = stay_duration_hist_bins)[0]
                 
+                if bandit[0].forager == 'IdealOptimal':
+                    results_all_sessions['matching_slope_IdealOptimal_theoretical_per_session'][ss] = bb.matching_slope_IdealOptimal_theoretical
+
             if not para_optim:
                 blockwise_stats_this_bandit[:, n_blocks_now : n_blocks_now + bb.n_blocks] =  \
                     np.vstack([bb.blockwise_choice_fraction, 
@@ -303,14 +308,16 @@ def run_sessions_parallel(bandit, n_reps = global_n_reps, pool = '', para_optim 
                     results_all_sessions['linear_fit_log_income_ratio'][unique_idx,:,:] = [np.nan, np.nan], [np.nan, np.nan],[np.nan, np.nan],[a, a_CI95]
                 except:
                     pass
-
-                
+            
     if not para_scan:    
         results_all_sessions['foraging_efficiency'] = np.array([np.mean(results_all_sessions['foraging_efficiency_per_session']),
                                                       1.96 * np.std(results_all_sessions['foraging_efficiency_per_session'])/np.sqrt(n_reps)])
         
         results_all_sessions['blockwise_stats'] = blockwise_stats_this_bandit # We need this only when not para_scan
         
+        if bandit[0].forager == 'IdealOptimal':        
+            results_all_sessions['matching_slope_IdealOptimal_theoretical'] = np.mean(results_all_sessions['matching_slope_IdealOptimal_theoretical_per_session'])
+
     # Basic info
     results_all_sessions['n_reps'] = n_reps
     results_all_sessions['forager'] = bandits_all_sessions[0].forager
@@ -494,6 +501,7 @@ def model_compet(model_compet_settings, n_reps = 200, pool = '', if_baited = Tru
     baseline_models = ['IdealOptimal','pMatching','IdealGreedy','Random']
     baseline_eff = []
     baseline_ms = []
+    ms_IO_analytical = 0   # Analytical matching slope of IdealOptimal
     
     for bm in baseline_models:
         bandit = Bandit(forager = bm, if_baited = if_baited, p_reward_sum = p_reward_sum, p_reward_pairs = p_reward_pairs)
@@ -505,9 +513,12 @@ def model_compet(model_compet_settings, n_reps = 200, pool = '', if_baited = Tru
 
         baseline_eff.append(results['foraging_efficiency'])
         baseline_ms.append([ms_mean, ms_CI95])
+        
+        if bm == 'IdealOptimal':
+            ms_IO_analytical += results['matching_slope_IdealOptimal_theoretical']
     
     plot_model_compet(model_compet_results, model_compet_settings, n_reps, 
-                      [baseline_models, baseline_eff, baseline_ms], 
+                      [baseline_models, baseline_eff, baseline_ms, ms_IO_analytical], 
                       if_baited = if_baited, p_reward_sum = p_reward_sum, p_reward_pairs = p_reward_pairs)
     
     
@@ -738,19 +749,18 @@ if __name__ == '__main__':  # This line is essential for apply_async to run in W
     # =============================================================================
             
     model_compet_settings = [
-        {'forager': 'LossCounting', 
-          'para_to_scan': {'loss_count_threshold_mean': np.hstack([0, 0,np.power(2,np.linspace(0,6,13)), np.inf])},  # 46.14626589
-          'para_to_fix': {'loss_count_threshold_std':  1.01529397}},
+        # {'forager': 'LossCounting', 
+        #   'para_to_scan': {'loss_count_threshold_mean': np.hstack([0, 0,np.power(2,np.linspace(0,6,13)), np.inf])},  # 46.14626589
+        #   'para_to_fix': {'loss_count_threshold_std':  1.01529397}},
         {'forager': 'Corrado2005', 
-          'para_to_scan': {'softmax_temperature': np.hstack([6.42381607e-02, np.power(10, np.linspace(-4,0,20))])},
+          'para_to_scan': {'softmax_temperature': np.hstack([6.42381607e-02, np.power(10, np.linspace(-4,0,15))])},
           'para_to_fix':  {'taus':  [1.83782228, 27.4467749], 'w_taus': [1-1.96842027e-02, 1.96842027e-02]}},
         {'forager': 'Bari2019', 
-          'para_to_scan': {'softmax_temperature': np.hstack([0.11987247, np.power(10, np.linspace(-1.5,0,20))])},  
+          'para_to_scan': {'softmax_temperature': np.hstack([0.11987247, np.power(10, np.linspace(-1.5,0,15))])},  
           'para_to_fix':  {'step_sizes': 0.49102943, 'forget_rate': 0.19255644}},
         ]
 
-    model_compet(model_compet_settings, n_reps = 10, p_reward_pairs = [[0.45, 0]], pool = pool) 
-    # model_compet(model_compet_settings, n_reps = 200, pool = pool)     
+    model_compet(model_compet_settings, n_reps = 20, pool = pool)     
         
     
     #%% Answer Sandro's question: what if optimizing all other parameters for each epsilon/sigma