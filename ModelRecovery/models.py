!! Fixed a huge bug in RL-like foragers / In previous codes (both model_fitting and test_bed), I used self.q_estimate to track Q, but I also used it to do a softmax. Then the Q(t+1) depends on the softmaxed Q(t) rather than the raw Q(t)... I fixed this bug by adding a dedicated self.choice_ps. THIS IS THE POINT OF PARAMETER RECOVERY!!!

ModelRecovery/models.py
@@ -45,7 +45,15 @@ def softmax(x,softmax_temperature):
         greedy[np.random.choice(np.where(x == np.max(x))[0])] = 1
         return greedy
     else:   # Normal softmax
-        return np.exp(x/softmax_temperature)/np.sum(np.exp(x/softmax_temperature))  # Accept np.arrays
+        return np.exp(x/softmax_temperature)/np.sum(np.exp(x/softmax_temperature))  # Accept np.
+    
+def choose_ps(ps):
+    '''
+    "Poisson"-choice process
+    '''
+    ps = ps/np.sum(ps)
+    return np.max(np.argwhere(np.hstack([-1e-16, np.cumsum(ps)]) < np.random.rand()))
+
 
 class BanditModels:
     
@@ -141,7 +149,10 @@ class BanditModels:
         
         # Initialization
         self.time = 0
-        
+        self.q_estimation = np.zeros([self.K, self.n_trials]) 
+        self.choice_prob = np.zeros([self.K, self.n_trials]) 
+        self.choice_prob[:] = 1/self.K   # To be strict
+         
         if self.if_fit_mode:  # Predictive mode
             self.predictive_choice_prob = np.zeros([self.K, self.n_trials])
             
@@ -159,8 +170,7 @@ class BanditModels:
         
         # Forager-specific
         if self.forager in ['RW1972_epsi','RW1972_softmax','Bari2019', 'Hattori2019']:
-            self.q_estimation = np.zeros([self.K, self.n_trials]) 
-            pass
+           pass
         
         elif self.forager in ['LNP_softmax']:
             # Compute the history filter. Compatible with any number of taus.
@@ -169,9 +179,6 @@ class BanditModels:
             
             for tau, w_tau in zip(self.taus, self.w_taus):
                 self.history_filter += w_tau * np.exp(-reversed_t / tau) / np.sum(np.exp(-reversed_t / tau))  # Note the normalization term (= tau when n -> inf.)
-            
-            self.q_estimation = np.zeros([self.K, self.n_trials]) # Estimation for each action (e.g., Q in Bari2019, L-stage scalar value in Corrado2005) 
-            self.q_estimation[:] = 1/self.K   # To be strict
             
         elif self.forager in ['LossCounting']:
             # Initialize
@@ -239,12 +246,6 @@ class BanditModels:
         # We should make it random afterwards
         np.random.seed()
         
-    def choose_ps(self, ps):
-        '''
-        "Poisson"-choice process
-        '''
-        ps = ps/np.sum(ps)
-        return np.max(np.argwhere(np.hstack([-1e-16, np.cumsum(ps)]) < np.random.rand()))
         
     def act_random(self):
         
@@ -310,7 +311,7 @@ class BanditModels:
         #     else:
         #         self.choice_history[0, self.time] = choice
             
-        # == The above is erroneous!! ==
+        # == The above is erroneous!! We should never realize any probabilistic events in model fitting!! ==
         choice = np.random.choice(np.where(self.q_estimation[:, self.time] == self.q_estimation[:, self.time].max())[0])
 
         if self.if_fit_mode:
@@ -327,11 +328,15 @@ class BanditModels:
 
     def act_Probabilistic(self):
         
+        # !! Should not change q_estimation!! Otherwise will affect following Qs
+        # And I put softmax here
+        self.choice_prob[:, self.time] = softmax(self.q_estimation[:, self.time], self.softmax_temperature)  
+
         if self.if_fit_mode:
-            self.predictive_choice_prob[:, self.time] = self.q_estimation[:, self.time]
+            self.predictive_choice_prob[:, self.time] = self.choice_prob[:, self.time]
             choice = None   # No need to make specific choice in fitting mode
         else:
-            choice = self.choose_ps(self.q_estimation[:, self.time])
+            choice = choose_ps(self.choice_prob[:, self.time])
             self.choice_history[0, self.time] = choice
             
         return choice
@@ -350,12 +355,12 @@ class BanditModels:
             else:
                 self.loss_count[0, self.time] = self.loss_count[0, self.time - 1] + 1
                 
-    def step_NLP(self,valid_reward_history):
+    def step_LNP(self,valid_reward_history):
         
         valid_filter = self.history_filter[-self.time:]
         local_income = np.sum(valid_reward_history * valid_filter, axis = 1)
         
-        self.q_estimation[:, self.time] = softmax(local_income, self.softmax_temperature)
+        self.q_estimation[:, self.time] = local_income
         
     def step_RWlike(self,choice,reward):
         
@@ -368,15 +373,16 @@ class BanditModels:
         # Choice-dependent forgetting rate ('Hattori2019')
         # Chosen:   Q(n+1) = (1- forget_rate_chosen) * Q(n) + step_size * (Reward - Q(n))
         self.q_estimation[choice, self.time] = (1 - self.forget_rates[1]) * self.q_estimation[choice, self.time - 1]  \
-                                         + learn_rate_this * (reward - self.q_estimation[choice, self.time - 1])
+                                              + learn_rate_this * (reward - self.q_estimation[choice, self.time - 1])
                                              
         # Unchosen: Q(n+1) = (1-forget_rate_unchosen) * Q(n)
         unchosen_idx = [cc for cc in range(self.K) if cc != choice]
         self.q_estimation[unchosen_idx, self.time] = (1 - self.forget_rates[0]) * self.q_estimation[unchosen_idx, self.time - 1] 
         
+        # --- The below three lines are erroneous!! Should not change q_estimation!! ---
         # Softmax in 'Bari2019', 'Hattori2019'
-        if self.forager in ['RW1972_softmax', 'Bari2019', 'Hattori2019']:
-            self.q_estimation[:, self.time] = softmax(self.q_estimation[:, self.time], self.softmax_temperature)
+        # if self.forager in ['RW1972_softmax', 'Bari2019', 'Hattori2019']:
+        #     self.q_estimation[:, self.time] = softmax(self.q_estimation[:, self.time], self.softmax_temperature)
 
        
     def act(self): # Compatible with either fitting mode (predictive) or not (generative). It's much clear now!!
@@ -433,7 +439,7 @@ class BanditModels:
             else:
                 valid_reward_history = self.reward_history[:, :self.time]   # Models' history till now
             
-            self.step_NLP(valid_reward_history)
+            self.step_LNP(valid_reward_history)
             
 
     def simulate(self):

ModelRecovery/run_model_recovery.py
@@ -295,34 +295,80 @@ if __name__ == '__main__':
     
     # -------------------------------------------------------------------------------------------
 
-    n_trials = 1000
+    # n_trials = 1000
 
-    forager = 'RW1972_epsi'
-    para_names = ['learn_rate_rew','epsilon']
-    para_scales = ['linear','linear']
-    para_bounds = [[0, 0],
-                    [1, 1]]
+    # forager = 'RW1972_epsi'
+    # para_names = ['learn_rate_rew','epsilon']
+    # para_scales = ['linear','linear']
+    # para_bounds = [[0, 0],
+    #                 [1, 1]]
+    
+    # #-- Para recovery
+    # n_models = 2
+    # true_paras = np.vstack((np.random.uniform(0, 1, size = n_models),
+    #                         np.random.uniform(0, 1, size = n_models),
+    #                         ))
+    # true_paras, fitted_para = fit_para_recovery(forager, 
+    #               para_names, para_bounds, true_paras, n_trials = n_trials, 
+    #               para_scales = para_scales, para_color_code = 1, para_2ds = [[0,1]],
+    #               fit_method = 'DE', pool = pool);    
+    
+    # # # -- LL_surface --
+    # # compute_LL_surface(forager, para_names, para_bounds, 
+    # #                     true_para = [0.1, 0.5],
+    # #                     para_2ds = [[0,1]], # LL surfaces for user-defined pairs of paras
+    # #                     n_grids = [[30,30]] * 6, 
+    # #                     para_scales = para_scales,
+    # #                     n_trials = n_trials,
+    # #                     fit_method = 'DE', n_x0s = 8, pool = pool)
+    
+    # -------------------------------------------------------------------------------------------
+    n_trials = 1000
+    
+    forager = 'RW1972_softmax'
+    para_names = ['learn_rate_rew','softmax_temperature']
+    para_scales = ['linear','log']
+    para_bounds = [[0, 1e-2],
+                    [1, 15]]
     
     #-- Para recovery
-    n_models = 2
+    n_models = 5
     true_paras = np.vstack((np.random.uniform(0, 1, size = n_models),
-                            np.random.uniform(0, 1, size = n_models),
+                            1/np.random.exponential(10, size = n_models),
                             ))
     true_paras, fitted_para = fit_para_recovery(forager, 
                   para_names, para_bounds, true_paras, n_trials = n_trials, 
                   para_scales = para_scales, para_color_code = 1, para_2ds = [[0,1]],
                   fit_method = 'DE', pool = pool);    
+
+    # # -------------------------------------------------------------------------------------------
+    # n_trials = 100
     
+    # forager = 'Bari2019'
+    # para_names = ['learn_rate_rew','forget_rate','softmax_temperature']
+    # para_scales = ['linear','linear','log']
+    # para_bounds = [[0, 0, 1e-2],
+    #                [1, 1, 15]]
+    
+    # # #-- Para recovery
+    # n_models = 5
+    # true_paras = np.vstack((np.random.uniform(0, 1, size = n_models),
+    #                         1/np.random.exponential(10, size = n_models),
+    #                         ))
+    # fit_para_recovery(forager, 
+    #               para_names, para_bounds, true_paras, n_trials = n_trials, 
+    #               para_scales = para_scales, para_color_code = 1, para_2ds = [[0,1]],
+    #               fit_method = 'DE', pool = pool);    
+
     # # -- LL_surface --
-    # compute_LL_surface(forager, para_names, para_bounds, 
-    #                     true_para = [0.1, 0.5],
-    #                     para_2ds = [[0,1]], # LL surfaces for user-defined pairs of paras
-    #                     n_grids = [[30,30]] * 6, 
-    #                     para_scales = para_scales,
-    #                     n_trials = n_trials,
-    #                     fit_method = 'DE', n_x0s = 8, pool = pool)
+    # # compute_LL_surface(forager, para_names, para_bounds, 
+    # #                     true_para = [0.1, 0.5],
+    # #                     para_2ds = [[0,1]], # LL surfaces for user-defined pairs of paras
+    # #                     n_grids = [[30,30]] * 6, 
+    # #                     para_scales = para_scales,
+    # #                     n_trials = n_trials,
+    # #                     fit_method = 'DE', n_x0s = 8, pool = pool)
     
-        
     #%%
     pool.close()   # Just a good practice
     pool.join()

Models/foraging_testbed_models.py
@@ -143,6 +143,7 @@ class Bandit:
         self.time = 0
         self.choice_history = np.zeros([1,self.n_trials])  # Choice history
         self.q_estimation = np.zeros([self.k, self.n_trials]) # Estimation for each action (e.g., Q in Bari2019, L-stage scalar value in Corrado2005) 
+        self.choice_prob = np.zeros([self.k, self.n_trials]) # Estimation for each action (e.g., Q in Bari2019, L-stage scalar value in Corrado2005) 
         self.reward_history = np.zeros([self.k, self.n_trials])    # Reward history, separated for each port (Corrado Newsome 2005)
         
         # Generate baiting prob in block structure
@@ -159,6 +160,8 @@ class Bandit:
             
             self.description = '%s, step_sizes = %s (tau_eff = %s), forget = %s, softmax_temp = %g, epsi = %g' % \
                                (self.forager, np.round(np.array(self.step_sizes),3), np.round(effective_taus,3), self.forget_rates, self.softmax_temperature, self.epsilon)
+                               
+            self.choice_prob[:] = 1/self.k   # To be strict
  
         elif self.forager in ['Sugrue2004', 'Corrado2005', 'Iigaya2019']:
             self.description = '%s, taus = %s, w_taus = %s, softmax_temp = %g, epsi = %g, random_before_total_reward = %g' % \
@@ -172,7 +175,7 @@ class Bandit:
             for tau, w_tau in zip(self.taus, self.w_taus):
                 self.history_filter += w_tau * np.exp(-reversed_t / tau) / np.sum(np.exp(-reversed_t / tau))  # Note the normalization term (= tau when n -> inf.)
             
-            self.q_estimation[:] = 1/self.k   # To be strict
+            self.choice_prob[:] = 1/self.k   # To be strict
             
         elif self.forager in ['LossCounting']:
             self.description = '%s, loss_thres +/- std = %g +/- %g' % (self.forager, self.loss_count_threshold_mean, self.loss_count_threshold_std)
@@ -396,13 +399,13 @@ class Bandit:
                     choice = np.random.choice(np.where(self.q_estimation[:, self.time] == self.q_estimation[:, self.time].max())[0])
                     
                 elif self.forager in ['Sugrue2004', 'Corrado2005', 'Iigaya2019', 'Bari2019', 'Hattori2019' ]:   # Poisson
-                    choice = self.choose_ps(self.q_estimation[:,self.time])    
+                    #  choice = self.choose_ps(self.q_estimation[:,self.time])    
+                    choice = self.choose_ps(self.choice_prob[:,self.time])    
                 
         self.choice_history[0, self.time] = int(choice)
         
         return int(choice)
-            
-  
+    
     def step(self, choice):
            
         # =============================================================================
@@ -483,9 +486,12 @@ class Bandit:
             unchosen_idx = [cc for cc in range(self.k) if cc != choice]
             self.q_estimation[unchosen_idx, self.time] = (1 - self.forget_rates[0]) * self.q_estimation[unchosen_idx, self.time - 1] 
             
+           
             # Softmax in 'Bari2019', 'Hattori2019'
             if self.forager in ['Bari2019', 'Hattori2019']:
-                self.q_estimation[:, self.time] = softmax(self.q_estimation[:, self.time], self.softmax_temperature)
+                # --- The below line is erroneous!! Should not change q_estimation!! 04/08/2020 ---
+                #     self.q_estimation[:, self.time] = softmax(self.q_estimation[:, self.time], self.softmax_temperature)
+                self.choice_prob[:, self.time] = softmax(self.q_estimation[:, self.time], self.softmax_temperature)
             
         elif self.forager in ['Sugrue2004', 'Iigaya2019']:
             # Fractional local income
@@ -499,10 +505,12 @@ class Bandit:
             # 2. Poisson choice probability = Fractional local income
             if np.sum(local_income) == 0:
                 # 50%-to-50%
-                self.q_estimation[:, self.time] = [1/self.k] * self.k
+                # self.q_estimation[:, self.time] = [1/self.k] * self.k
+                self.choice_prob[:, self.time] = [1/self.k] * self.k
             else:
                 # Local fractional income
-                self.q_estimation[:, self.time] = local_income / np.sum(local_income)
+                # self.q_estimation[:, self.time] = local_income / np.sum(local_income)
+                self.choice_prob[:, self.time] = local_income / np.sum(local_income)
                 
         elif self.forager == 'Corrado2005':
             # Softmaxed local income
@@ -513,7 +521,8 @@ class Bandit:
             local_income = np.sum(valid_reward_history * valid_filter, axis = 1)
             
             # 2. Poisson choice probability = Softmaxed local income (Note: Equivalent to "difference + sigmoid" in [Corrado etal 2005], for 2lp case)
-            self.q_estimation[:, self.time] = softmax(local_income, self.softmax_temperature)
+            # self.q_estimation[:, self.time] = softmax(local_income, self.softmax_temperature)
+            self.choice_prob[:, self.time] = softmax(local_income, self.softmax_temperature)
             
         return reward
   